9ad4b8a6f50b000ff4f35845954992b7284d8b26
ARamKHachatryan5 <aram.khachatryan293@gmail.com>
2019-02-14 22:06:13 +0400

    new

--- js/script.js
+++ js/script.js
@@ -1,25 +1,7 @@
-// const color = ["#315c68", "#d3412d", "black"]
-// let i = 0;
-// setInterval(() => {
-//     $(".divWelcome h1").css({
-//         color: color[i]
-//     });
-//     i++
-//     if (i > 2) { i = 0 }
-// }, 200)
-// $.fn.isInViewport = function() {
-//     var elementTop = $(this).offset().top;
-//     var elementBottom = elementTop + $(this).outerHeight();
-
-//     var viewportTop = $(window).scrollTop();
-//     var viewportBottom = viewportTop + $(window).height();
-
-//     return elementBottom > viewportTop && elementTop < viewportBottom;
-// };
-// $(window).on('resize scroll', function() {
-//     if ($('.divTwo').isInViewport()) {
-//         console.log('dff')
-//     } else {
-//         // do something else
-//     }
-// });
+const click = (tag, pixel) => {
+    $(tag).on("click", () => {
+        $("#menuDiv").css({ "margin-right": pixel });
+    });
+}
+click("#menu", "0px");
+click("#cancel", "-300px");
