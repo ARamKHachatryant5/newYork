9e2a149ef8fd2e72b7df58ae7500a60ea54e57a0
ARamKHachatryant5 <aram.khachatryan293@gmail.com>
2019-02-27 16:53:04 +0400

    FixedBugs

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
 $(document).ready(function () {
-    const click = (tag, pixel) => {
+    const click = (tag, pixel, zindex, transition) => {
         $(tag).on("click", () => {
             $("#menuDiv").css({ "margin-right": pixel });
+            $(".elem4").css({ "z-index": zindex, "transition": `${transition}s` });
+
         });
     }
-    click("#menu", "0px");
-    click("#cancel", "-300px");
+    click("#menu", "0px", "-1","0");
+    click("#cancel", "-300px", "1","1");
     $(window).scroll(() => {
         $("header").css({ "background-color": "brown" })
-    });
-    $(".owl-carousel").owlCarousel({
-        items: 3,
-        nav: true,
-        margin: 10,
-        navText: ["<i class='fa fa-chevron-left fa-6x'></i>", "<i class='fa fa-chevron-right fa-6x'></i>"]
     });
     $(".elem2 span").typed({
         strings: [
@@ -25,4 +21,9 @@ $(document).ready(function () {
         backDelay: 200,
         loop: true
     })
+    $('.elem4').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
 });
